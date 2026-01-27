f63771c8dcf4bf7154b3d5632e4e8c0e891417da
theweich <dagisanz@gmail.com>
2026-01-27 09:08:53 +0100

    Added backtracking examples and cosmetic changes

--- recursion/prints_n_range.cpp
+++ recursion/prints_n_range.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 void print_range_n(int, int);
 void print_range_n_reverse(int, int);
+void print_range_n_backtracking(int, int);
+void print_range_n_reverse_backtracking(int, int);
 
 int main(int argc, char const *argv[])
 {
     print_range_n(1, 30);
     print_range_n_reverse(1, 30);
+    print_range_n_backtracking(1, 30);
+    print_range_n_reverse_backtracking(1, 30);
     return 0;
 }
 
@@ -27,7 +31,7 @@ void print_range_n(int i, int n)
 
 void print_range_n_reverse(int i, int n)
 {
-    if (n < i)
+    if (i > n)
     {
         cout << endl;
         return;
@@ -36,4 +40,30 @@ void print_range_n_reverse(int i, int n)
     cout << n << " ";
 
     print_range_n_reverse(i, n - 1);
+}
+
+void print_range_n_backtracking(int i, int n)
+{
+    if (i > n)
+    {
+        cout << endl;
+        return;
+    }
+
+    print_range_n_backtracking(i, n - 1);
+
+    cout << n << " ";
+}
+
+void print_range_n_reverse_backtracking(int i, int n)
+{
+    if (i > n)
+    {
+        cout << endl;
+        return;
+    }
+
+    print_range_n_reverse_backtracking(i + 1, n);
+
+    cout << i << " ";
 }
