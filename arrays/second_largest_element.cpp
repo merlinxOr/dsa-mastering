2ea8a8988e8d29ab59ac9171892f2cac47f130cf
theweich <dagisanz@gmail.com>
2026-02-04 11:48:17 +0100

    second_largest_element.cpp

---
+++ arrays/second_largest_element.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int second_largest_element(int *, int);
+
+int main(int argc, char const *argv[])
+{
+
+    int arr[] = {1, 2, 4, 7, 7, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << second_largest_element(arr, size) << endl;
+
+    return 0;
+}
+
+int second_largest_element(int *arr, int n)
+{
+    int largest = arr[0];
+    int second_largest = -1;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (largest < arr[i])
+        {
+            largest = arr[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > second_largest && arr[i] < largest)
+        {
+            second_largest = arr[i];
+        }
+    }
+
+    return second_largest;
+}
