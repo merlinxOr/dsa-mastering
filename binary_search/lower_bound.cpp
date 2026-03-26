7e379f7fa21acda264b3a39472068fe6a3129b99
theweich <dagisanz@gmail.com>
2026-03-26 09:31:29 +0100

    lower_bound.cpp

---
+++ binary_search/lower_bound.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lower_bound_bf(vector<int> &nums, int target);
+int lower_bound_optimal(vector<int> &nums, int target);
+int lower_bound_stl(vector<int> &nums, int target);
+
+int main()
+{
+    vector<int> nums = {2, 3, 7, 10, 11, 11, 25};
+    int target = 9;
+
+    int res = lower_bound_optimal(nums, target);
+
+    cout << res << endl;
+}
+
+/*
+ * Time complexity = O(n)
+ * Space complexity = O(1)
+ */
+int lower_bound_bf(vector<int> &nums, int target)
+{
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] >= target)
+            return i;
+    }
+
+    return n;
+}
+
+/*
+ * Time complexity = O(log n)
+ * Space complexity = O(1)
+ */
+int lower_bound_optimal(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    int res = nums.size();
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] >= target)
+        {
+            res = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    return res;
+}
+
+/*
+ * Time complexity = O(log n)
+ * Space complexity = O(1)
+ */
+int lower_bound_stl(vector<int> &nums, int target)
+{
+    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+}
